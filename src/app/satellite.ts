a2997455766802b409cd00a7932d062976738545
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-07-06 20:39:55 -0400

    Highlight Space Debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,6 +5,7 @@ export class Satellite {
 	launchDate: string;
 	orbitType: string;
 	operational: boolean;
+	
 
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
@@ -15,7 +16,13 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
+		
+		if(this.type === "Space Debris") {
+		
 		return true;
+		} else {
+			 return false;
+		}
    }
 
 }
